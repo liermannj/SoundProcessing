e9bcaf3445a43e222b6da920cddb3621fb23096f
liermannj <jerome_liermann@carrefour.com>
2020-04-05 15:51:12 +0200

    fix - sound: make the process terminate
    - add actor termination after target data line closing

--- sound/src/main/scala/com/jliermann/sound/Boot.scala
+++ sound/src/main/scala/com/jliermann/sound/Boot.scala
@@ -3,13 +3,28 @@ package com.jliermann.sound
 import akka.actor.ActorSystem
 import com.jliermann.sound.environment.EnvironmentLive
 import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.LazyLogging
 
-private[sound] object Boot extends App with AudioApp {
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.io.StdIn
+import scala.util.{Failure, Success, Try}
+
+private[sound] object Boot extends App with AudioApp with LazyLogging {
+  implicit val actorSystem: ActorSystem = ActorSystem()
   val rootConfig = RootConfiguration.loadConfigOrThrow(ConfigFactory.load())
   for {
     tdl <- getTargetDataLine(rootConfig.soundConfiguration.audioFormat)
   } {
-    implicit val actorSystem: ActorSystem = ActorSystem()
     Job.run(EnvironmentLive, rootConfig, tdl)
+    logger.info("Speak into the microphone to start recording...")
+    logger.info("Press enter to stop...")
+    StdIn.readLine()
+    logger.info("Wait for the flow to end...")
+    Try(Await.result(actorSystem.terminate(), 10.second)) match {
+      case Success(_) => logger.info("Flow terminated successfully")
+      case Failure(exception) => throw exception
+    }
   }
+
 }
--- sound/src/test/scala/com/jliermann/sound/RootConfigurationSpec.scala
+++ sound/src/test/scala/com/jliermann/sound/RootConfigurationSpec.scala
@@ -1,9 +1,9 @@
 package com.jliermann.sound
 
 import com.typesafe.config.ConfigFactory
-import org.scalatest.{FlatSpec, Matchers, TryValues}
+import org.scalatest.{FlatSpec, Matchers}
 
-class RootConfigurationSpec extends FlatSpec with Matchers with TryValues {
+class RootConfigurationSpec extends FlatSpec with Matchers {
 
   "loadConfig" should "correctly load the configuration" in {
     noException shouldBe thrownBy(RootConfiguration.loadConfigOrThrow(ConfigFactory.load("referenceSpec.conf").resolve()))
